5b1156eeb6333b298c1ec1ba954ecc3e6543415a
Anggasta <anggaazhari12@gmail.com>
2021-11-29 15:51:34 +0700

    Tambah spasi

--- resources/views/AmplopCoklat/index.blade.php
+++ resources/views/AmplopCoklat/index.blade.php
@@ -19,7 +19,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
       <div class="container">
-        <a href="#" id="judul" class="navbar-brand">AmplopCoklat</a>
+        <a href="#" id="judul" class="navbar-brand">Amplop Coklat</a>
 
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
