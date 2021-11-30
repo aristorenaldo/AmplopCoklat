0600958e99f0ecb617477f40d018d227d8b33805
devina24 <devinadwipr@gmail.com>
2021-11-30 23:01:15 +0700

    melihat

--- resources/views/AmplopCoklat/melihatinfo.blade.php
+++ resources/views/AmplopCoklat/melihatinfo.blade.php
@@ -79,12 +79,18 @@
         <div class="col-lg-6 py-3 wow fadeInUp">
             <h3>Pendaftar:</h3>
             <div class="border rounded-lg p-5">
-                <ul class="mb-0">
+                <div class="row">
+                    <div class="col-6 ">
+                        
+                    </div>
+
+                </div>
+                <!-- <ul class="mb-0">
                     <li>Anggasta Aji Azhari</li>
                     <div class="divider"></div>
                     <li>Anggasta Aji Azhari</li>
                     <div class="divider"></div>
-                </ul>
+                </ul> -->
             </div>
         </div>
     </div>
