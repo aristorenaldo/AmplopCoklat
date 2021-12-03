dca35db03653a6c346a7fa2398e1c4e1b452cce2
Anggasta <anggaazhari12@gmail.com>
2021-12-03 20:03:53 +0700

    Oke

--- resources/views/AmplopCoklat/index.blade.php
+++ resources/views/AmplopCoklat/index.blade.php
@@ -19,7 +19,7 @@
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="500">
       <div class="container">
-        <a href="#" id="judul" class="navbar-brand">Amplop Coklat</a>
+        <a href="#" id="judul" class="navbar-brand">AmplopCoklat</a>
 
         <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -34,7 +34,16 @@
               <a class="nav-link" href="about.html">About</a>
             </li>
             <li class="nav-item">
-              <a class="btn btn-primary2 ml-lg-2" href="#">Sign Up</a>
+              <a class="nav-link" href="service.html">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="blog.html">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-primary ml-lg-2" href="#">Sign Up</a>
             </li>
           </ul>
         </div>
@@ -45,17 +54,12 @@
     <div class="container">
       <div class="page-banner home-banner">
         <div id="banner" class="row align-items-center flex-wrap-reverse h-100">
-          <div class="col-md-6 py-5 wow fadeInLeft">
+          <div class="col-md-12 py-5 wow fadeInLeft">
             <h1 class="mb-4">Find Your Dream Job From Your Uniqueness</h1>
-            <p class="tagline">Disabilitas Bisa Kerja</p>
+            <p id="tagline">Disabilitas Bisa Kerja</p>
+            <img src="disability.png" alt="Gambar Disability">
             <div class="search">
               <input type="text" class="searchTerm" placeholder="Cari Lowongan Kerja">
-              <span><img class="logo-search" src="/images/searchicon.png" alt="Logo-Search" width = "20" height = "20"></span>
-            </div>
-          </div>
-          <div class="col-md-62 py-5 wow zoomIn">
-            <div class="img-fluid mt-5">
-              <img src="images/homepic.png" alt="Gambar-Home" width = "490" height = "369">
             </div>
           </div>
         </div>
@@ -65,51 +69,43 @@
   </header>
 
   <div class="page-section">
-    <div class="text-center wow fadeInUp">
-      <div class="subhead2">Lowongan Pekerjaan Tersedia</div>
-      <div class="divider mx-auto"></div>
-    </div>
     <div class="container">
-      <div class="row" >
-        <div class="col-lg-4" id="card-loker1">
+      <div class="row">
+        <div class="col-lg-4">
           <div class="card-service wow fadeInUp">
+            <div class="header">
+              <img src="../assets/img/services/service-1.svg" alt="">
+            </div>
             <div class="body">
               <img id="logo-google" src="/images/Google.png">
               <h5 class="text-secondary">UI Designer</h5>
-              <p class="text-secondary-bottom">Google LLC</p> 
-              <div>
-                <img src="/images/location.png" id="gambar-location" width="15" height="20">
-                <span clas="nama-lokasi">Bandung</span>
-                <span class="gaji">Rp. 8jt/bulan</span>
-              </div>
+              <p class="text-secondary-bottom">Google LLC</p>
               <a href="service.html" class="btn btn-primary">Details</a>
             </div>
           </div>
         </div>
-        <div class="card-service wow fadeInUp">
-          <div class="body">
-            <img id="logo-google" src="/images/gojek.png">
-            <h5 class="text-secondary">Android Developer</h5>
-            <p class="text-secondary-bottom">Gojek</p> 
-            <div>
-              <img src="/images/location.png" id="gambar-location" width="15" height="20">
-              <span clas="nama-lokasi">Jakarta</span>
-              <span class="gaji">Rp. 10jt/bulan</span>
+        <div class="col-lg-4">
+          <div class="card-service wow fadeInUp">
+            <div class="header">
+              <img src="../assets/img/services/service-2.svg" alt="">
             </div>
-            <a href="service.html" class="btn btn-primary">Details</a>
+            <div class="body">
+              <h5 class="text-secondary">Content Marketing</h5>
+              <p>We help you define your SEO objective & develop a realistic strategy with you</p>
+              <a href="service.html" class="btn btn-primary">Read More</a>
+            </div>
           </div>
         </div>
-        <div class="card-service wow fadeInUp">
-          <div class="body">
-            <img id="logo-google" src="/images/tokopedia.png">
-            <h5 class="text-secondary">Data Engineer</h5>
-            <p class="text-secondary-bottom">Tokopedia</p> 
-            <div>
-              <img src="/images/location.png" id="gambar-location" width="15" height="20">
-              <span clas="nama-lokasi">Jakarta</span>
-              <span class="gaji">Rp. 15jt/bulan</span>
+        <div class="col-lg-4">
+          <div class="card-service wow fadeInUp">
+            <div class="header">
+              <img src="../assets/img/services/service-3.svg" alt="">
             </div>
-            <a href="service.html" class="btn btn-primary">Details</a>
+            <div class="body">
+              <h5 class="text-secondary">Keyword Research</h5>
+              <p>We help you define your SEO objective & develop a realistic strategy with you</p>
+              <a href="service.html" class="btn btn-primary">Read More</a>
+            </div>
           </div>
         </div>
       </div>
@@ -120,23 +116,24 @@
     <div class="container">
       <div class="row align-items-center">
         <div class="col-lg-6 py-3 wow fadeInUp">
-          <span class="subhead">TENTANG KAMI</span>
-          <h2 class="title-section">Platform Kerja Untuk Disabilitas</h2>
+          <span class="subhead">About Us</span>
+          <h2 class="title-section">The number #1 SEO Service Company</h2>
           <div class="divider"></div>
+
           <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
           <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren.</p>
-          <a href="about.html" id="button-about" class="btn btn-primary mt-3">Read More</a>
+          <a href="about.html" class="btn btn-primary mt-3">Read More</a>
         </div>
         <div class="col-lg-6 py-3 wow fadeInRight">
           <div class="img-fluid py-3 text-center">
-            <img id="gambar-about" src="images/about.png" alt="gambar-aboutus" width = "388" height = "426">
+            <img src="../assets/img/about_frame.png" alt="">
           </div>
         </div>
       </div>
     </div> <!-- .container -->
   </div> <!-- .page-section -->
 
-  <!-- LOWONGAN KERJA
+  <!-- LOWONGAN KERJA -->
   <div class="page-section">
     <div class="container">
       <div class="text-center wow fadeInUp">
@@ -193,13 +190,13 @@
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
 
-  <div class="page-section2">
+  <div class="page-section bg-light">
     <div class="container">
       <div class="text-center wow fadeInUp">
-        <div class="subhead2">MITRA KERJA KAMI</div>
-        <h2 class="title-section">You Can Find Job From Our Mitra</h2>
+        <div class="subhead">Our services</div>
+        <h2 class="title-section">How SEO Team Can Help</h2>
         <div class="divider mx-auto"></div>
       </div>
 
@@ -209,7 +206,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Dana</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -218,7 +215,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>OVO</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -227,7 +224,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Lazada</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -236,7 +233,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Tokopedia</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -245,7 +242,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Blibli</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -254,7 +251,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Bibit</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -263,7 +260,7 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Google</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
@@ -272,13 +269,143 @@
               <div class="header mb-3">
                 <span class="mai-business"></span>
               </div>
-              <h5>Gojek</h5>
+              <h5>OnSite SEO</h5>
               <p>We analyse your website's structure, internal architecture & other key</p>
             </div>
           </div>
         </div>
-    </div>
-  </div>
+
+    </div> <!-- .container -->
+  </div> <!-- .page-section -->
+
+  <div class="page-section banner-seo-check">
+    <div class="wrap bg-image" style="background-image: url(../assets/img/bg_pattern.svg);">
+      <div class="container text-center">
+        <div class="row justify-content-center wow fadeInUp">
+          <div class="col-lg-8">
+            <h2 class="mb-4">Check your Website SEO</h2>
+            <form action="#">
+              <input type="text" class="form-control" placeholder="E.g google.com">
+              <button type="submit" class="btn btn-success">Check Now</button>
+            </form>
+          </div>
+        </div>
+      </div> <!-- .container -->
+    </div> <!-- .wrap -->
+  </div> <!-- .page-section -->
+
+  <div class="page-section">
+    <div class="container">
+      <div class="text-center wow fadeInUp">
+        <div class="subhead">Pricing Plan</div>
+        <h2 class="title-section">Choose plan the right for you</h2>
+        <div class="divider mx-auto"></div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-lg-4 py-3 wow zoomIn">
+          <div class="card-pricing">
+            <div class="header">
+              <div class="pricing-type">Basic</div>
+              <div class="price">
+                <span class="dollar">$</span>
+                <h1>39<span class="suffix">.99</span></h1>
+              </div>
+              <h5>Per Month</h5>
+            </div>
+            <div class="body">
+              <p>25 Analytics <span class="suffix">Campaign</span></p>
+              <p>1,300 Change <span class="suffix">Keywords</span></p>
+              <p>Social Media <span class="suffix">Reviews</span></p>
+              <p>1 Free <span class="suffix">Optimization</span></p>
+              <p>24/7 <span class="suffix">Support</span></p>
+            </div>
+            <div class="footer">
+              <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 py-3 wow zoomIn">
+          <div class="card-pricing marked">
+            <div class="header">
+              <div class="pricing-type">Standar</div>
+              <div class="price">
+                <span class="dollar">$</span>
+                <h1>59<span class="suffix">.99</span></h1>
+              </div>
+              <h5>Per Month</h5>
+            </div>
+            <div class="body">
+              <p>25 Analytics <span class="suffix">Campaign</span></p>
+              <p>1,300 Change <span class="suffix">Keywords</span></p>
+              <p>Social Media <span class="suffix">Reviews</span></p>
+              <p>1 Free <span class="suffix">Optimization</span></p>
+              <p>24/7 <span class="suffix">Support</span></p>
+            </div>
+            <div class="footer">
+              <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-4 py-3 wow zoomIn">
+          <div class="card-pricing">
+            <div class="header">
+              <div class="pricing-type">Professional</div>
+              <div class="price">
+                <span class="dollar">$</span>
+                <h1>99<span class="suffix">.99</span></h1>
+              </div>
+              <h5>Per Month</h5>
+            </div>
+            <div class="body">
+              <p>25 Analytics <span class="suffix">Campaign</span></p>
+              <p>1,300 Change <span class="suffix">Keywords</span></p>
+              <p>Social Media <span class="suffix">Reviews</span></p>
+              <p>1 Free <span class="suffix">Optimization</span></p>
+              <p>24/7 <span class="suffix">Support</span></p>
+            </div>
+            <div class="footer">
+              <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div> <!-- .container -->
+  </div> <!-- .page-section -->
+
+  <!-- Banner info -->
+  <div class="page-section banner-info">
+    <div class="wrap bg-image" style="background-image: url(../assets/img/bg_pattern.svg);">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 py-3 pr-lg-5 wow fadeInUp">
+            <h2 class="title-section">SEO to Improve Brand <br> Visibility</h2>
+            <div class="divider"></div>
+            <p>We're an experienced and talented team of passionate consultants who breathe with search engine marketing.</p>
+            
+            <ul class="theme-list theme-list-light text-white">
+              <li>
+                <div class="h5">SEO Content Strategy</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+              <li>
+                <div class="h5">B2B SEO</div>
+                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6 py-3 wow fadeInRight">
+            <div class="img-fluid text-center">
+              <img src="../assets/img/banner_image_2.svg" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> <!-- .wrap -->
+  </div> <!-- .page-section -->
+
 
   <footer class="page-footer bg-image" style="background-image: url(../assets/img/world_pattern.svg);">
     <div class="container">
